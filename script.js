ac58db54cb29226dfeb9de2c552ab2915f73ff71
alexiscameronfrankel <alexiscameronfrankel@gmail.com>
2020-02-07 16:39:57 -0500

    setting up canvas and index.html

---
+++ script.js
@@ -0,0 +1,16 @@
+//setting up the height and width of the canvas...just setting up the canvas in general//
+
+const canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+let width = window.innerWidth
+let height = window.innerHeight
+
+////////////////////////////////
+
+
+
+ctx.fillRect(60, 80, window.innerWidth, window.innerHeight);
+
+
+
+
