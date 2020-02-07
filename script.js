f217fd61251c59e051b417ecd2ab69af85361ba2
alexiscameronfrankel <alexiscameronfrankel@gmail.com>
2020-02-07 17:03:07 -0500

    setting it to the full height and width of the window

--- script.js
+++ script.js
@@ -2,14 +2,19 @@
 
 const canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-let width = window.innerWidth
-let height = window.innerHeight
+let windowWidth = window.innerWidth
+let windowHeight = window.innerHeight
 
-////////////////////////////////
+//code below actually sets the dimmensions to the full height and width of the window
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+
+//////////////////////////////////////////
 
 
 
-ctx.fillRect(60, 80, window.innerWidth, window.innerHeight);
+ctx.fillRect(60, 80, canvas.width, canvas.height);
 
 
 
